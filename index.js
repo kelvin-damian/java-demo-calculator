330511db7708dcf92ad21b00f2b656f68fa88879
kelvin-damian <104898078+kelvin-damian@users.noreply.github.com>
2022-06-24 21:29:58 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&family=Source+Sans+Pro&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/reach.css">
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="mainHeader">
+                <div class="logo">
+                    <a href="index.html"><img src="images/kev.png" alt="Home brand logo" srcset=""></a>
+                </div>
+                <div class="rightLink">
+                    <div class="navigation">
+                        <nav>
+                            <ul>
+                                <li><a href="about.html">About</a></li>
+                                <li><a href="contact.html">Contact</a></li>
+                                <li><a href="registration.html">Registration</a></li>
+                            </ul>
+                        </nav>
+                        <nav class="login">
+                            <ul>
+                                <li class="login"><a href="login.html">Login</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            <h1>Contact us</h1>
+            <hr>
+            <p>DROP US A LINE
+                <p>
+        </div>
+
+    </header>
+    <main>
+        <section id="pageContainer">
+            <div class="formContainer">
+                <form action="">
+                    <div class="formParagraph">
+                        <div class="forminput">
+                            <input type="text" placeholder="Name"><br>
+                            <input type="text" placeholder="Email"><br>
+                            <textarea name="your-message" id="textarea" cols="40" rows="10" placeholder="Message"></textarea><br>
+                            <input type="submit" value="send" class="button">
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="rightContainer">
+                <div class="image">
+                    <img src="images/Screenshot.png" alt="google map">
+                </div>
+            </div>
+        </section>
+    </main>
+    <!--pageFooter-->
+    <footer id="pageFooter">
+        <div class="footer">
+            <div class="logo">
+                <img src="images/kelvin logo.png" alt="Home brand logo" srcset="">
+                <p>Make your bookings and lets collaborate on that magical product</p>
+            </div>
+            <div class="address">
+                <div class="img">
+                    <img src="images/my_location.png" alt="location logo">
+                </div>
+                <p>St Gregory Road, South-west Ikoyi, Lagos.</p>
+            </div>
+            <div class="address">
+                <div class="img">
+                    <img src="images/mail.png" alt="Email logo">
+                </div>
+                <div class="textBox">
+                    <a href="mailto:nkworkelvin007@gmail.com">nkworkelvin007@gmail.com</a>
+                </div>
+            </div>
+            <div class="address">
+                <div class="img">
+                    <img src="images/call.png" alt="call logo">
+                </div>
+                <p>phone: 08131547106</p>
+            </div>
+        </div>
+        <div class="footer2">
+            <h3>Links</h3>
+            <nav>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Contact Us</a></li>
+                    <li><a href="registration.html">Registration</a></li>
+                    <li><a href="login.html">Login</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="footer3">
+            <h3>Socials</h3>
+            <div class="socials">
+                <div class="socialImage">
+                    <div class="log1">
+                        <a href="https://web.facebook.com/kelvin.nkwor.7/"><img src="images/facebook-logo.png" alt="facebook logo"></a>
+                    </div>
+                    <div class="log1">
+                        <a href="https://www.linkedin.com/in/kelvinnkwor/"><img src="images/linkedin-logo.png" alt="linkedin logo"></a>
+                    </div>
+                    <div class="log1">
+                        <a href="https://twitter.com/Kelvindamiann"><img src="images/twitter-logo-.png" alt="twitter logo"></a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </footer>
+
+</body>
+
+</html>
